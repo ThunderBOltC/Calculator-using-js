2b1034bda5392b90bbd00afd2888fef749e6775d
ThunderBoltC <chandon1072@gmail.com>
2024-11-19 01:03:22 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert("Invalid Expression");
+        clearDisplay();
+    }
+}
